d9b4374fe78b9f7c5e08cad25d29cc15407e0222
SuperGHOST-42 <ariclenesp12@gmail.com>
2026-08-23 00:46:38 +0100

    add: Module_01

---
+++ Module_01/ex00/Zombie.cpp
@@ -0,0 +1,17 @@
+#include "Zombie.hpp"
+#include <iostream>
+
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+
+Zombie::~Zombie()
+{
+	std::cout << this->name << " Destroyed" << std::endl;
+}
+
+void Zombie::announce()
+{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..."<< std::endl;
+}
