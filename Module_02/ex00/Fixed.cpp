ab1130e3144694c072835401533f2935ab4eeef5
Ariclenes Pereira <arpereir@c1r6s12.42lisboa.com>
2026-08-25 21:09:20 +0100

    add: Module_02/ex00

---
+++ Module_02/ex00/Fixed.cpp
@@ -0,0 +1,36 @@
+#include "Fixed.hpp"
+#include <iostream>
+
+Fixed::Fixed(void) : value(0)
+{
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed(void)
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed &Fixed::operator=(const Fixed &copy)
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	value = copy.value;
+	return (*this);
+}
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->value);
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	this->value = raw;
+}
