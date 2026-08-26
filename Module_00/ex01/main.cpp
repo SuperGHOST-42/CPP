a212cf6d5bf35ccb326430adc054f72add2cd13e
Ariclenes Pereira <arpereir@c1r5s3.42lisboa.com>
2026-08-26 19:37:21 +0100

    add: clearScreen()

--- Module_00/ex01/main.cpp
+++ Module_00/ex01/main.cpp
@@ -1,25 +1,36 @@
 #include "PhoneBook.hpp"
 #include <iostream>
+#include <cstdlib>
 
 int main(void)
 {
     PhoneBook phoneBook;
     std::string command;
 
+    bool clearScrean = true;
+
     while (1)
     {
+        if (clearScrean == true)
+            std::system("clear");
+        
+        clearScrean = true;
+        
         std::cout << "Enter command (ADD, SEARCH, EXIT): ";
-
+        
         if (!std::getline(std::cin, command))
             break;
-
+        
+        std::system("clear");
+        
         if (command == "ADD")
         {
             if (phoneBook.addContact() == 1)
-                return (1);
+            return (1);
         }
         else if (command == "SEARCH")
         {
+            clearScrean = false;
             if (phoneBook.search() == 1)
                 return (1);
         }
